272ba4cd780d7d1bc54c6cb4d7fc329bddd3c1b8
MEIGUICHAO <499976081@qq.com>
2017-07-10 15:03:13 +0800

    Initial commit

---
+++ app/src/main/java/com/itworks/simulatelottery/MainActivity.java
@@ -0,0 +1,157 @@
+package com.itworks.simulatelottery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResult;
+    private int allcount;
+    private ArrayList<int[]> positionList;
+    private ArrayList<int[]> numList;
+    private ArrayList<int[]> templepositionList;
+    private ArrayList<int[]> templenumList;
+    private ArrayList<ArrayList<Integer>> allLists;
+    private int BLANK_INT = 95;
+    private int BEGIN_INT = 200;
+
+
+    private void initBaseData() {
+
+        if (null == allLists) {
+            allLists = new ArrayList<>();
+        }
+
+        positionList = initList();
+        numList = initList();
+        templepositionList = initList();
+        templenumList = initList();
+
+    }
+
+    private ArrayList<int[]> initList() {
+        ArrayList<int[]> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int[] ints = new int[10];
+            for (int j = 0; j < 10; j++) {
+                ints[j] = 0;
+            }
+            list.add(ints);
+        }
+        return list;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        tvResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLotteryResult();
+            }
+        });
+
+    }
+
+    private void getLotteryResult() {
+        allcount = 0;
+
+        initBaseData();
+
+        for (int m = 0; m < 1; m++) {
+            for (int j = 0; j < 20000; j++) {
+                ArrayList<Integer> integers = new ArrayList<>();
+                Set<Integer> resultSet = generateRandomArray(10);
+                Iterator<Integer> it = resultSet.iterator();
+
+
+                if (j > BEGIN_INT) {
+                    initBaseData();
+                    caculateBlankData();
+                }
+
+
+
+                //result
+                while (it.hasNext()) {
+                    integers.add(it.next());
+                }
+                allLists.add(integers);
+            }
+
+        }
+
+        Log.e("", "getLotteryResult: " + allcount);
+
+
+
+    }
+
+    private void caculateBlankData() {
+        getBlankDate(BLANK_INT);
+    }
+
+    private void getBlankDate(int blank) {
+        for (int i = 0; i < allLists.size(); i++) {
+            for (int j = 0; j < 10; j++) {
+                getPositionData(allLists.get(i).get(j), j);
+            }
+        }
+        for (int i = 0; i < allLists.size()-blank; i++) {
+            for (int j = 0; j < 10; j++) {
+                getTemplePositionData(allLists.get(i).get(j),j);
+            }
+        }
+        for (int i = 0; i < positionList.size(); i++) {
+            for (int j = 0; j < 10; j++) {
+                if (positionList.get(i)[j] == templepositionList.get(i)[j]) {
+                    allcount++;
+                    Log.e("allcount", "getBlankDateAllcount: " + allcount);
+                    tvResult.setText("" + allcount);
+                }
+            }
+        }
+
+    }
+
+    private void getTemplePositionData(int integer, int i) {
+        templepositionList.get(i)[integer]++;
+    }
+
+    private void getNumData(Integer integer, int i) {
+        numList.get(integer)[i]++;
+    }
+
+    private void getPositionData(Integer integer, int i) {
+        positionList.get(i)[integer]++;
+    }
+
+    public static Set<Integer> generateRandomArray(int size){
+
+        Set<Integer> set = new LinkedHashSet<Integer>(); //集合是没有重复的值,LinkedHashSet是有顺序不重复集合,HashSet则为无顺序不重复集合
+        Integer num = size;
+        Integer range = size;
+        Random ran = new Random();
+        while(set.size() < num){
+            Integer tmp = ran.nextInt(range); //0-51之间随机选一个数
+            set.add(tmp);//直接加入，当有重复值时，不会往里加入，直到set的长度为52才结束
+        }
+        return set;
+    }
+
+
+
+}
